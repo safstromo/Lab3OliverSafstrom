ddc18a69def8d70f3f86cc4914c93b04a3829403
Oliver <safstrom.oliver@gmail.com>
2022-10-18 16:52:23 +0200

    Cleanup imports

--- src/main/java/se/iths/lab3oliversafstrom/MainApplication.java
+++ src/main/java/se/iths/lab3oliversafstrom/MainApplication.java
@@ -5,13 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Polygon;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;

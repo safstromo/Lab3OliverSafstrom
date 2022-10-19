c4901d6e74c98cd3b1cc02d175baa8d21905fbac
Oliver <safstrom.oliver@gmail.com>
2022-10-19 13:58:15 +0200

    add onEnter,SendMessage

--- src/main/java/se/iths/lab3oliversafstrom/Controller.java
+++ src/main/java/se/iths/lab3oliversafstrom/Controller.java
@@ -1,19 +1,42 @@
 package se.iths.lab3oliversafstrom;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
 import se.iths.lab3oliversafstrom.shapes.Circle;
 import se.iths.lab3oliversafstrom.shapes.Rectangle;
 
 public class Controller {
+    @FXML
+    private Button sendButton;
+    @FXML
+    private TextField chatBoxInput;
+    @FXML
+    private ListView chatWindow;
+    @FXML
+    private Button selectShape;
+    @FXML
+    private Button RectangleButton;
+    @FXML
+    private Button circleButton;
+    @FXML
+    private Canvas canvas;
+    @FXML
+    private ColorPicker colorPicker;
+    @FXML
+    private String chatBoxMessage;
 
-    double mouseX;
-    double mouseY;
-    ColorPicker colorPicker;
+    public void initialize() {
+        model = new Model();
+        colorPicker = new ColorPicker();
 
-    //TODO initialize method()
+    }
+
     private Model model = new Model();
 
     public void connectServer(ActionEvent actionEvent) {
@@ -49,16 +72,34 @@ public class Controller {
     }
 
     public void canvasClicked(MouseEvent mouseEvent) {
-        this.mouseX = mouseEvent.getSceneX();
-        this.mouseY = mouseEvent.getSceneY();
-        System.out.println(mouseX);
-        System.out.println(mouseY);
+        model.setMouseX(mouseEvent.getSceneX());
+        model.setMouseY(mouseEvent.getSceneY());
+        System.out.println(model.getMouseX());
+        System.out.println(model.getMouseY());
         if (drawCircleButton()) {
-            Circle circle = new Circle(10, mouseX, mouseY,colorPicker.getValue());
+            createNewCircle();
         } else if (drawRectangleButton()) {
-            Rectangle rectangle = new Rectangle(10, mouseX, mouseY,colorPicker.getValue());
+            createNewRectangle();
         }
+    }
 
+    private void createNewRectangle() {
+        Rectangle rectangle = new Rectangle(10, model.getMouseX(), model.getMouseY(), model.getColorPicker().getValue());
+    }
 
+    private void createNewCircle() {
+        Circle circle = new Circle(10, model.getMouseX(), model.getMouseY(), model.getColorPicker().getValue());
+    }
+
+    public void sendMessage() {
+        model.setChatBoxInput(chatBoxInput.getText());
+        System.out.println(model.getChatBoxInput());
+        chatBoxInput.setText("");
+    }
+
+    public void onEnter() {
+        model.setChatBoxInput(chatBoxInput.getText());
+        System.out.println(model.getChatBoxInput());
+        chatBoxInput.setText("");
     }
 }
--- src/main/java/se/iths/lab3oliversafstrom/Model.java
+++ src/main/java/se/iths/lab3oliversafstrom/Model.java
@@ -1,4 +1,62 @@
 package se.iths.lab3oliversafstrom;
 
+
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ListView;
+
 public class Model {
+    private double mouseX;
+    private double mouseY;
+    private ColorPicker colorPicker;
+    private String chatBoxInput;
+    private ListView chatWindow;
+
+    public String getChatBoxInput() {
+        return chatBoxInput;
+    }
+
+    public void setChatBoxInput(String chatBoxInput) {
+        this.chatBoxInput = chatBoxInput;
+    }
+
+    public ListView getChatWindow() {
+        return chatWindow;
+    }
+
+    public void setChatWindow(ListView chatWindow) {
+        this.chatWindow = chatWindow;
+    }
+
+    public double getMouseX() {
+        return mouseX;
+    }
+
+    public void setMouseX(double mouseX) {
+        this.mouseX = mouseX;
+    }
+
+    public double getMouseY() {
+        return mouseY;
+    }
+
+    public void setMouseY(double mouseY) {
+        this.mouseY = mouseY;
+    }
+
+    public ColorPicker getColorPicker() {
+        return colorPicker;
+    }
+
+    public void setColorPicker(ColorPicker colorPicker) {
+        this.colorPicker = colorPicker;
+    }
+
+    public Model(double mouseX, double mouseY, ColorPicker colorPicker) {
+        this.mouseX = mouseX;
+        this.mouseY = mouseY;
+        this.colorPicker = colorPicker;
+    }
+
+    public Model() {
+    }
 }
